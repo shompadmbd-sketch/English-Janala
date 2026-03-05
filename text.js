f67afbf98c3838c93c14ed165067854569910092
shompadmbd-sketch <shompadmbd@gmail.com>
2026-03-05 19:55:36 +0600

    english janala with api

---
+++ text.js
@@ -0,0 +1,8 @@
+
+const createElements = (arr)=>{
+    // console.log(arr);
+    const htmlElements = arr.map(el => `<span class = "btn">${el}</span>`);
+    console.log(htmlElements.join(" "));
+};
+const synonyms=["hello", "hi" , "konnichiwa"];
+createElements(synonyms);
